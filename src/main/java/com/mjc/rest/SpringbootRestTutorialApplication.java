fb955d04a065d61ccfe2a63263c8c1d53abc6b84
Prasanna Manjunatha <clhprasanna@gmail.com>
2020-03-03 14:57:01 -0500

    Springboot rest tutorial

---
+++ src/main/java/com/mjc/rest/SpringbootRestTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.mjc.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestTutorialApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/mjc/rest/SpringbootRestTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
